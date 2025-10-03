e760b0753b3fd677c28086ff264ebb95e60904b1
Kren Bence <Kren.Bence@BOLYAI.LOCAL>
2025-10-03 12:23:10 +0200

    javascript alapok

---
+++ table_col/script.js
@@ -0,0 +1,34 @@
+console.log("gomszab a kedvenc tanárom")
+const a = "szia"
+/**
+ * @type{string}
+ */
+console.log(a)
+
+const b = ["a","b","c"]
+console.log(b)
+for(let i=0;i<b.length;i++){
+    console.log(b[i])
+}
+console.log(b[1])
+
+/** 
+ * while()
+ * 
+ * do
+ * while()
+*/
+for(const key in b){
+
+}
+
+for(const key in b){
+    console.log(`${key}:${b[key]}$`)
+}
+
+/**@type{{name:string, age:number}} */
+
+const y ={
+    name:'bence',
+    age:17
+}
